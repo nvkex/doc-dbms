95e35dc483ec34033bc0ee2e90689b8aebe2c0ec
nvkex <scrbwrm@gmail.com>
2021-04-11 13:17:00 +0530

    entry value validators

---
+++ validators/value.validator.js
@@ -0,0 +1,13 @@
+const { MAX_BUFFER_LENGTH } =  require("../constants");
+
+const checkValueSize = (value) => {
+    const size = Buffer.byteLength(JSON.stringify(value)) / 1024;
+    if (size > MAX_BUFFER_LENGTH)
+        throw "Value cannot be greater than " + MAX_BUFFER_LENGTH + "."
+    else
+        return true;
+}
+
+module.exports = {
+    checkValueSize
+}
