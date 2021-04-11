0bbeab8ed482b0c706033b967b3662486595e79f
nvkex <scrbwrm@gmail.com>
2021-04-11 13:16:09 +0530

    constants

---
+++ constants.js
@@ -0,0 +1,13 @@
+const DEFAULT_PATH = './';
+const MAX_KEY_LENGTH = 32;
+const MAX_BUFFER_LENGTH = 16;
+const MAX_STORE_SIZE = 1048576;
+const STORE_NAME = 'store.db'
+
+module.exports = {
+    DEFAULT_PATH,
+    MAX_KEY_LENGTH,
+    MAX_BUFFER_LENGTH,
+    MAX_STORE_SIZE,
+    STORE_NAME
+}
