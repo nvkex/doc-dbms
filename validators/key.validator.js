8fa2ec5ab6976eeb89a4765acbb0c37b912a78fb
nvkex <scrbwrm@gmail.com>
2021-04-11 13:16:38 +0530

    Entry key validators

---
+++ validators/key.validator.js
@@ -0,0 +1,27 @@
+const { MAX_KEY_LENGTH } =  require("../constants");
+
+
+const validateKeySize = (key) => {
+    if (key.length > MAX_KEY_LENGTH)
+        throw "Key cannot be of more than " + MAX_KEY_LENGTH + " characters.";
+    else
+        return true;
+}
+
+const validateKeyLife = (data) => {
+    if(!data)
+        throw "Key doesn't exist in store!";
+    if(data.timeToLive == null)
+        return 
+    
+    const currentTime = parseInt(new Date().getTime()/1000);
+    if(currentTime - data.iat > data.timeToLive)
+        throw "Key expired. Cannot operate on this key."
+    else
+        return true
+}
+
+module.exports = {
+    validateKeySize,
+    validateKeyLife
+}
